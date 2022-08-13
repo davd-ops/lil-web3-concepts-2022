0ecebdfcb783199baf4389e9f296ae43ccaceab6
davd-ops <davidpsencik459@gmail.com>
2022-08-14 01:58:02 +0200

    base functionality done

--- contracts/ERC721/IPFS/metadata_generator/generation.ts
+++ contracts/ERC721/IPFS/metadata_generator/generation.ts
@@ -4,6 +4,7 @@ const sharp = require('sharp');
 const PROJECT_NAME = 'TEST PROJECT'
 
 type MetadataObject = {
+    trait_type: string;
     name: string,
     image: string,
     weight: number
@@ -11,31 +12,37 @@ type MetadataObject = {
 
 const background:MetadataObject[] = [
     {
+        trait_type: 'background',
         name: 'Blue Background',
         image: './traits/background/blue.png',
         weight: 1500,
     },
     {
+        trait_type: 'background',
         name: 'Green Background',
         image: './traits/background/green.png',
         weight: 1500,
     },
     {
+        trait_type: 'background',
         name: 'Pink Background',
         image: './traits/background/pink.png',
         weight: 1500,
     },
     {
+        trait_type: 'background',
         name: 'Golden Background',
         image: './traits/background/golden.png',
         weight: 200,
     },
     {
+        trait_type: 'background',
         name: 'Demon Background',
         image: './traits/background/demon.png',
         weight: 200,
     },
     {
+        trait_type: 'background',
         name: 'Zombie Background',
         image: './traits/background/zombie.png',
         weight: 100,
@@ -44,31 +51,37 @@ const background:MetadataObject[] = [
 
 const body:MetadataObject[] = [
     {
+        trait_type: 'body',
         name: 'White skin',
         image: './traits/body/white_body.png',
         weight: 1500,
     },
     {
+        trait_type: 'body',
         name: 'Dark skin',
         image: './traits/body/dark_body.png',
         weight: 1500,
     },
     {
+        trait_type: 'body',
         name: 'Yellow skin',
         image: './traits/body/yellow_body.png',
         weight: 1500,
     },
     {
+        trait_type: 'body',
         name: 'Demon skin',
         image: './traits/body/demon_body.png',
         weight: 200,
     },
     {
+        trait_type: 'body',
         name: 'Avatar skin',
         image: './traits/body/avatar_body.png',
         weight: 200,
     },
     {
+        trait_type: 'body',
         name: 'Zombie skin',
         image: './traits/body/zombie_body.png',
         weight: 100,
@@ -77,31 +90,37 @@ const body:MetadataObject[] = [
 
 const clothes:MetadataObject[] = [
     {
+        trait_type: 'clothes',
         name: 'White tee',
         image: './traits/clothes/white_tee.png',
         weight: 1500,
     },
     {
+        trait_type: 'clothes',
         name: 'Dark tee',
         image: './traits/clothes/dark_tee.png',
         weight: 1500,
     },
     {
+        trait_type: 'clothes',
         name: 'Yellow tee',
         image: './traits/clothes/yellow_tee.png',
         weight: 1500,
     },
     {
+        trait_type: 'clothes',
         name: 'Red hoodie',
         image: './traits/clothes/red_hoodie.png',
         weight: 200,
     },
     {
+        trait_type: 'clothes',
         name: 'Blue hoodie',
         image: './traits/clothes/blue_hoodie.png',
         weight: 200,
     },
     {
+        trait_type: 'clothes',
         name: 'Green hoodie',
         image: './traits/clothes/green_hoodie.png',
         weight: 100,
@@ -110,31 +129,37 @@ const clothes:MetadataObject[] = [
 
 const hat:MetadataObject[] = [
     {
+        trait_type: 'hat',
         name: 'None',
         image: './traits/none_trait.png',
         weight: 1500,
     },
     {
+        trait_type: 'hat',
         name: 'Just a hat',
         image: './traits/hat/just_a_hat.png',
         weight: 1500,
     },
     {
+        trait_type: 'hat',
         name: 'No-cap Cap',
         image: './traits/hat/nocap_cap.png',
         weight: 1500,
     },
     {
+        trait_type: 'hat',
         name: 'Bucket of water',
         image: './traits/hat/bucket_of_water.png',
         weight: 200,
     },
     {
+        trait_type: 'hat',
         name: 'Magicians hat',
         image: './traits/hat/magicians_hat.png',
         weight: 200,
     },
     {
+        trait_type: 'hat',
         name: 'Beanie',
         image: './traits/hat/beanie.png',
         weight: 100,
@@ -143,31 +168,37 @@ const hat:MetadataObject[] = [
 
 const accessories:MetadataObject[] = [
     {
+        trait_type: 'accessories',
         name: 'None',
         image: './traits/none_trait.png',
         weight: 1500,
     },
     {
+        trait_type: 'accessories',
         name: 'Yellow Earring',
         image: './traits/accessories/yellow_earring.png',
         weight: 1500,
     },
     {
+        trait_type: 'accessories',
         name: 'Blue Earring',
         image: './traits/accessories/blue_earring.png',
         weight: 1500,
     },
     {
+        trait_type: 'accessories',
         name: 'Tattoos',
         image: './traits/accessories/tattoos.png',
         weight: 200,
     },
     {
+        trait_type: 'accessories',
         name: '3D glasses',
         image: './traits/accessories/3d_glasses.png',
         weight: 200,
     },
     {
+        trait_type: 'accessories',
         name: 'Laser eyes',
         image: './traits/accessories/laser_eyes.png',
         weight: 100,
@@ -201,9 +232,7 @@ const selectRandomTraitUsingWeightedRarities = (options: Array<MetadataObject>) 
 }
 
 const generateImage = async (traits: Array<MetadataObject>, tokenId: number) => {    
-        console.log('traits[0]');
-        console.log(traits[0]);
-        
+
         sharp(traits[0].image) //the background
         .composite([ //input all traits here
             { input: traits[1].image },
@@ -228,12 +257,24 @@ const generateMetadata = async (traits: Array<MetadataObject>, tokenId: number) 
         image: '', 
         attributes: [
             {
-                trait_type: '', 
+                trait_type: traits[0].trait_type, 
                 value: traits[0].image,
             }, 
             {
-                trait_type: '', 
-                value: '',
+                trait_type: traits[1].trait_type, 
+                value: traits[1].image,
+            }, 
+            {
+                trait_type: traits[2].trait_type, 
+                value: traits[2].image,
+            }, 
+            {
+                trait_type: traits[3].trait_type, 
+                value: traits[3].image,
+            }, 
+            {
+                trait_type: traits[4].trait_type, 
+                value: traits[4].image,
             }, 
         ], 
     }
@@ -269,8 +310,6 @@ const generate = async (numberOfTokens: number) => {
 console.log(
     '\n\n******************************** \n\n        PROGRAM INITIATED \n\n ********************************\n\n'
 )
-
-let random = Math.random() * 18
 
 generate(2)
    
