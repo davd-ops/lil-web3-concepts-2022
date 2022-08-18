c00958971ec0817e5ee5b91798a4792203df1b8b
davd-ops <davidpsencik459@gmail.com>
2022-08-18 22:15:56 +0200

    work on ERC1155 initiated

---
+++ contracts/ERC1155/ERC1155MintingContract.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "hardhat/console.sol";
+
+contract ERC1155MintingContract is ERC1155, Ownable {
+
+}
