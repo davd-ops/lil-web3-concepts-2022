1f7d3015423a67e64202f01c2ac5bf512b448568
davd-ops <davidpsencik459@gmail.com>
2022-08-24 00:43:52 +0200

    generating SVG onchain added

--- contracts/ERC721/on-chain/OnChainERC721.sol
+++ contracts/ERC721/on-chain/OnChainERC721.sol
@@ -24,6 +24,8 @@ contract OnChainERC721 is ERC721, Ownable {
     error MismatchedInput();
     error TokenNotMinted();
 
+    using Strings for uint32;
+
     bool public SALE_OPEN; 
     uint32 immutable public MAX_MINT_PER_ADDRESS; 
     uint32 immutable public MAX_SUPPLY; 
@@ -63,7 +65,7 @@ contract OnChainERC721 is ERC721, Ownable {
     mapping (address => uint32) public mintedAmountByAddress; 
 
     // mapping from tokenId to a struct containing the token's traits
-    mapping(uint32 => Animal) public tokenTraits;
+    mapping(uint32 => Animal) internal tokenTraits;
 
     // mapping from hashed(tokenTrait) to the tokenId it's associated with
     // used to ensure there are no duplicates
@@ -92,6 +94,7 @@ contract OnChainERC721 is ERC721, Ownable {
 
     /**
         * mints new tokens through a public sale
+        * @dev add generation of metadata
     */
     function mint() payable external {
         if (!SALE_OPEN) revert SaleNotOpen();
@@ -145,34 +148,6 @@ contract OnChainERC721 is ERC721, Ownable {
         }
     }
 
-     /**
-        * generates an entire SVG by composing multiple <image> elements of PNGs
-        * @param _tokenId the ID of the token to generate an SVG for
-        * @return a valid SVG of the animal
-    */
-    function drawSVG(uint32 _tokenId) public view returns (string memory) {
-        if (!_exists(_tokenId)) revert TokenNotMinted();
-        Animal memory traits = tokenTraits[_tokenId];
-
-        string memory svgString = string(abi.encodePacked(
-            drawTrait(traitData[0][traits.fur]),
-            drawTrait(traitData[1][traits.head]),
-            drawTrait(traitData[2][traits.ears]),
-            drawTrait(traitData[3][traits.eyes]),
-            drawTrait(traitData[4][traits.nose]),
-            drawTrait(traitData[5][traits.mouth]),
-            drawTrait(traitData[6][traits.feet])
-        ));
-
-        console.log(traits.head);
-
-        return string(abi.encodePacked(
-            '<svg id="woolf" width="100%" height="100%" version="1.1" viewBox="0 0 40 40" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">',
-                svgString,
-            "</svg>"
-        ));
-    }
-
     /**
          * generates an <image> element using base64 encoded PNGs
          * @param _trait the trait storing the PNG data
@@ -184,5 +159,157 @@ contract OnChainERC721 is ERC721, Ownable {
                 _trait.png,
             '"/>'
         ));
+    }
+
+     /**
+        * generates an entire SVG by composing multiple <image> elements of PNGs
+        * @param _tokenId the ID of the token to generate an SVG for
+        * @return a valid SVG of the animal
+        * @dev it is possible that there are no traits assigned now
+    */
+    function drawSVG(uint32 _tokenId) internal view returns (string memory) {
+        Animal memory token = tokenTraits[_tokenId];
+
+        string memory svgString = string(abi.encodePacked(
+            drawTrait(traitData[0][token.fur]),
+            drawTrait(traitData[1][token.head]),
+            drawTrait(traitData[2][token.ears]),
+            drawTrait(traitData[3][token.eyes]),
+            drawTrait(traitData[4][token.nose]),
+            drawTrait(traitData[5][token.mouth]),
+            drawTrait(traitData[6][token.feet])
+        ));
+
+        return string(abi.encodePacked(
+            '<svg id="animal" width="100%" height="100%" version="1.1" viewBox="0 0 40 40" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">',
+                svgString,
+            "</svg>"
+        ));
+    }
+
+    /**
+         * generates an attribute for the attributes array in the ERC721 metadata standard
+         * @param traitType the trait type to reference as the metadata key
+         * @param value the token's trait associated with the key
+         * @return a JSON dictionary for the single attribute
+     */
+    function attributeForTypeAndValue(string memory traitType, string memory value) internal pure returns (string memory) {
+        return string(abi.encodePacked(
+            '{"trait_type":"',
+                traitType,
+                   '","value":"',
+                value,
+            '"}'
+        ));
+    }
+
+    /**
+         * generates an array composed of all the individual traits and values
+         * @param _tokenId the ID of the token to compose the metadata for
+         * @return a JSON array of all of the attributes for given token ID
+     */
+    function compileAttributes(uint32 _tokenId) internal view returns (string memory) {
+        
+        Animal memory token = tokenTraits[_tokenId];
+        string memory traits;
+        traits = string(abi.encodePacked(
+            attributeForTypeAndValue(_traitTypes[0], traitData[0][token.fur].name),',',
+            attributeForTypeAndValue(_traitTypes[1], traitData[1][token.head].name),',',
+            attributeForTypeAndValue(_traitTypes[2], traitData[2][token.ears].name),',',
+            attributeForTypeAndValue(_traitTypes[3], traitData[3][token.eyes].name),',',
+            attributeForTypeAndValue(_traitTypes[4], traitData[4][token.nose].name),',',
+            attributeForTypeAndValue(_traitTypes[5], traitData[5][token.mouth].name),',',
+            attributeForTypeAndValue(_traitTypes[6], traitData[6][token.feet].name),','
+        ));
+        return string(abi.encodePacked(
+            '[',
+                traits,
+            ']'
+        ));
+    }
+
+    /**
+         * generates a base64 encoded metadata response without referencing off-chain content
+         * @param _tokenId the ID of the token to generate the metadata for
+         * @return a base64 encoded JSON dictionary of the token's metadata and SVG
+     */
+    function tokenURI(uint256 _tokenId) public view override returns (string memory) {
+        if (!_exists(_tokenId)) revert TokenNotMinted();
+
+        uint32 tokenId = uint32(_tokenId);
+
+        string memory metadata = string(abi.encodePacked(
+            '{"name": "',
+            'Animal #',
+            tokenId.toString(),
+            '", "description": "Colletion of ERC721 tokens with fully on-chain metadata.", "image": "data:image/svg+xml;base64,',
+            base64(bytes(drawSVG(tokenId))),
+            '", "attributes":',
+            compileAttributes(tokenId),
+            "}"
+        ));
+
+        return string(abi.encodePacked(
+            "data:application/json;base64,",
+            base64(bytes(metadata))
+        ));
+    }
+
+    /** BASE 64 library - Written by Brech Devos */
+  
+    string internal constant TABLE = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/';
+
+    function base64(bytes memory data) internal pure returns (string memory) {
+        if (data.length == 0) return '';
+        
+        // load the table into memory
+        string memory table = TABLE;
+
+        // multiply by 4/3 rounded up
+        uint256 encodedLen = 4 * ((data.length + 2) / 3);
+
+        // add some extra buffer at the end required for the writing
+        string memory result = new string(encodedLen + 32);
+
+        assembly {
+            // set the actual output length
+            mstore(result, encodedLen)
+            
+            // prepare the lookup table
+            let tablePtr := add(table, 1)
+            
+            // input ptr
+            let dataPtr := data
+            let endPtr := add(dataPtr, mload(data))
+            
+            // result ptr, jump over length
+            let resultPtr := add(result, 32)
+            
+            // run over the input, 3 bytes at a time
+            for {} lt(dataPtr, endPtr) {}
+            {
+                dataPtr := add(dataPtr, 3)
+                
+                // read 3 bytes
+                let input := mload(dataPtr)
+                
+                // write 4 characters
+                mstore(resultPtr, shl(248, mload(add(tablePtr, and(shr(18, input), 0x3F)))))
+                resultPtr := add(resultPtr, 1)
+                mstore(resultPtr, shl(248, mload(add(tablePtr, and(shr(12, input), 0x3F)))))
+                resultPtr := add(resultPtr, 1)
+                mstore(resultPtr, shl(248, mload(add(tablePtr, and(shr( 6, input), 0x3F)))))
+                resultPtr := add(resultPtr, 1)
+                mstore(resultPtr, shl(248, mload(add(tablePtr, and(        input,  0x3F)))))
+                resultPtr := add(resultPtr, 1)
+            }
+            
+            // padding with '='
+            switch mod(mload(data), 3)
+            case 1 { mstore(sub(resultPtr, 2), shl(240, 0x3d3d)) }
+            case 2 { mstore(sub(resultPtr, 1), shl(248, 0x3d)) }
+        }
+        
+        return result;
     }
 }
